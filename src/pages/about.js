188fbff5c256cec79392b9f59c8c7ed93999d5c0
Kayla Sween <kaylasween@Kaylas-MacBook-Pro.local>
2019-05-21 21:55:01 -0500

    Adding picture and starting to work on about.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import deadlift from '../images/IMG_7742.jpeg'
+
 const AboutPage = () => (
   <Layout>
     <h1>About</h1>
+    <img src={deadlift} alt="Kayla deadlifting 298lbs." />
     
   </Layout>
 )
