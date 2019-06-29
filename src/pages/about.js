a4e73f8f750dfdc44063693b2dfea8f9d5c118cd
Kayla Sween <kayla@kaylasween.com>
2019-06-29 15:52:55 -0500

    adding link for extra life. removing target=_blanks for user preference on opening in new tab.

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,10 +15,10 @@ const AboutPage = () => (
       </Col>
       <Col>
         <p>Kayla is a web developer who is passionate about front-end design and development. She is an advocate for user experience,
-        especially accessibility. She has spoken at <a href="https://magnoliajs.com" target="_blank">MagnoliaJS</a> about accessibility.</p>
-        <p>In her spare time, she enjoys learning new front-end technologies. Currently, she is learning <a href="" target="_blank">Gatsby</a>.</p>
+        especially accessibility. She has spoken at <a href="https://magnoliajs.com">MagnoliaJS</a> about accessibility.</p>
+        <p>In her spare time, she enjoys learning new front-end technologies. Currently, she is learning <a href="">Gatsby</a>.</p>
         <p>She also enjoys travelling with her husband, Richard. </p>
-        <p>She is a gamer and enjoys supporting <a href="" target="_blank">Extra Life</a>.</p>
+        <p>She is a gamer and enjoys supporting <a href="https://www.extra-life.org/participant/305822">Extra Life</a>.</p>
         <p>She has two dogs, Captain and Piper.</p>
         <p>Kayla also competes in powerlifting. Her current best lifts are 275.5lbs/125kg in the squat, 187.4lbs/85kg in the bench press,
         and 319.6lbs/145kg in the deadlift. She is also a certified <a href="https://coaching.usapowerlifting.com/certified-coaches/">USA Powerlifting Club Coach</a>.</p>
