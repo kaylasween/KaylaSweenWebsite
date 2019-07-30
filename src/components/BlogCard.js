2a2a7ebf06746e98343a49942656ea807b202fc6
Kayla Sween <kayla@kaylasween.com>
2019-07-30 15:00:37 -0500

    cleaning up excess CSS and formatting

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -12,22 +12,21 @@ import './blogCard.css'
 
 const BlogCard = ({ post }) => (
   <Card className="blog-card"> 
-    {/* <Card.Img variant="top" src={post.social_image} alt={"Links to Kayla's post" + post.title + " on the Dev Community"} /> */}
-      <Card.Title>
-        <a href={'https://dev.to/kaylasween/' + post.slug}>
-          <h3>{post.title}</h3>
+    <Card.Title>
+      <a href={'https://dev.to/kaylasween/' + post.slug}>
+        <h3>{post.title}</h3>
+      </a>
+    </Card.Title>
+    <Card.Subtitle>
+      Published on {post.readable_publish_date} 
+    </Card.Subtitle>
+    <Card.Link className="tags">
+      {post.tag_list_array.map((tag, key) => (
+        <a key={key} href={'https://dev.to/t/' + tag}>
+          <span className="tag">{`#${tag}`}</span>
         </a>
-      </Card.Title>
-      <Card.Subtitle>
-        Published on {post.readable_publish_date} 
-      </Card.Subtitle>
-      <Card.Link className="tags">
-        {post.tag_list_array.map((tag, key) => (
-          <a key={key} href={'https://dev.to/t/' + tag}>
-            <span className="tag">{`#${tag}`}</span>
-          </a>
-        ))}
-      </Card.Link>
+      ))}
+    </Card.Link>
   </Card>
 )
 
