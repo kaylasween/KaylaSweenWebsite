4c9f77751838205bb6434e3a2cbef84841dda9c7
Kayla Sween <kayla@kaylasween.com>
2019-07-30 17:53:10 -0500

    nope

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `font-awesome`, 
     {
       resolve: "gatsby-source-dev",
       options: {
