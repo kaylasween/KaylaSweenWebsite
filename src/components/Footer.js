ade0fccbee2d23d032b4f00067df1d5d1c42a91c
Kayla Sween <kayla@kaylasween.com>
2019-07-30 18:51:29 -0500

    changing font awesome icons to svgs that I am actually using

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import Dev from '../images/dev-brands.svg'
+import GitHub from '../images/github-brands.svg'
+import Twitter from '../images/twitter-brands.svg'
+
 import './footer.css';
 
 const Footer = () => (
   <footer>
     <a href="https://github.com/kaylasween">
-      <FontAwesomeIcon icon={['fab', 'github']} alt="Link to Kayla's Github" />
+      <img src={GitHub} alt="Link to Kayla's Github" className="footer-logo" />
     </a>
     <a href="https://dev.to/kaylasween">
-      <FontAwesomeIcon icon={['fab', 'dev']} alt="Link to Kayla's Dev Community page" />
+      <img src={Dev} alt="Link to Kayla's Dev Community page" className="footer-logo" />
     </a>
     <a href="https://twitter.com/_KaylaSween">
-      <FontAwesomeIcon icon={['fab', 'twitter']} alt="Link to Kayla's Twitter page" />
+      <img src={Twitter} alt="Link to Kayla's Twitter page" className="footer-logo" />
     </a>
     <div className="float-right">
       © {new Date().getFullYear()} Kayla Sween, Built with
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,6 @@ import Layout from '../components/Layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-config.autoAddCss = true
-
-library.add(fab, fas)
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`Kayla`, `Sween`, `Kayla Sween`, `Web Development`]} />
