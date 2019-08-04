50a0a38db3da57e4da8b5a74471fdbf8379e41ca
Kayla Sween <kayla@kaylasween.com>
2019-08-04 16:46:32 -0500

    adding stuff to talks page.

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -4,13 +4,18 @@ import Layout from '../components/Layout'
 const Talks = () => (
   <Layout>
     <h1>Talks</h1>
+    <p>I typically post slides for my talks after they happen on my GitHub.</p>
     <h2>Conferences</h2>
-    <p><a href="https://magnoliajs.com">MagnoliaJS</a></p>
-    
-    <p><a href="https://promptconf.com">PromptConf</a></p>
-    <p><a href="https://www.renderatl.com">Render(atl)</a></p>
+    <h3><a href="https://magnoliajs.com">MagnoliaJS</a></h3>
+    <p>I spoke about avoiding SUX, or some user experience, at MagnoliaJS in 2019. You can find the slides for that on my GitHub. </p>
+    <h3><a href="https://promptconf.com">PromptConf</a></h3>
+    <p>I'll be speaking about the four principles of accessibility in a lightning talk at PrompConf in September 2019.</p>
+    <h3><a href="https://www.renderatl.com">Render(ATL)</a></h3>
+    <p>I'll be speaking at Render(ATL) in May 2020. I hope to see you there!</p>
     <h2>Meetups</h2>
-    <p><a href="https://jacksondevs.tech">Jackson Area Web and App Developers Meetup</a></p>
+    <h3><a href="https://jacksondevs.tech">Jackson Area Web and App Developers (JAWAD) Meetup</a></h3>
+    <p>You can find the <a href="https://github.com/kaylasween/JAWAD-Pour/blob/master/pour.png">slides for my talk about P.O.U.R.</a> at the July 2019 JAWAD 
+    Meetup on Github.</p>
   </Layout>
 )
 
