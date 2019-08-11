b96bdb84dbf4dee9b933eebf6f01b0f0df7f669a
Kayla Sween <kayla@kaylasween.com>
2019-08-11 18:37:40 -0500

    adding an image card. Will add overlay between text and screenshot to increase contrast.

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types'
 
 import Card from 'react-bootstrap/Card'
 
+import placeholder from '../images/gatsby-astronaut.png'
+
 const Project = () => (
-    <Card>
-        
+    <Card className="bg-dark">
+        <Card.Img src={placeholder} alt="Card image" />
+        <Card.ImgOverlay>
+            <Card.Title>Project title</Card.Title>
+            <Card.Text>
+                This is where the project decription will go.
+            </Card.Text>
+            <Card.Text>approximate date</Card.Text>
+        </Card.ImgOverlay>
     </Card>
 )
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Project from '../components/Project'
 
 const ProjectPage = () => (
   <Layout>
     <h1>Projects</h1>
     <p>I'll put some cards in here with screen captures and links if applicable.</p>
+    <Project />
   </Layout>
 )
 
