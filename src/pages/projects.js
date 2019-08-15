6ee6e8d48f738506711c4d6af6bc12bbcf41cbd3
Kayla Sween <kayla@kaylasween.com>
2019-08-15 15:09:52 -0500

    making the project card smaller.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,27 @@
 import React from 'react'
+
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
 import Layout from '../components/Layout'
 import Project from '../components/Project'
 
 const ProjectPage = () => (
   <Layout>
-    <h1>Projects</h1>
-    <p>I'll put some cards in here with screen captures and links if applicable.</p>
-    <Project />
+    <Container>
+      <Row>
+        <Col>
+          <h1>Projects</h1>
+          <p>I'll put some cards in here with screen captures and links if applicable.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={4}>
+          <Project />
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
