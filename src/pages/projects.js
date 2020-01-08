ec1efb928dbab83a1b674dec2f91c8d4aeffe776
Kayla Sween <kayla@kaylasween.com>
2020-01-08 16:58:46 -0600

    adding links to my github and links to specific talks.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 const ProjectPage = () => (
   <Layout>
     <h1>Projects</h1>
-    <p><em>List of projects coming soon!</em></p>
+    <p><em>List of projects coming soon!</em> You can check out what I'm currently working on on <a href="https://github.com/kaylasween">my GitHub</a>.</p>
   </Layout>
 )
 
--- src/pages/talks.js
+++ src/pages/talks.js
@@ -4,10 +4,10 @@ import Layout from '../components/Layout'
 const Talks = () => (
   <Layout>
     <h1>Talks</h1>
-    <p>I typically post slides for my talks after they happen on my GitHub.</p>
+    <p>I typically post slides for my talks after they happen on <a href="https://github.com/kaylasween">my GitHub</a>.</p>
     <h2>Conferences</h2>
     <h3><a href="https://magnoliajs.com">MagnoliaJS</a></h3>
-    <p>I spoke about avoiding SUX, or some user experience, at MagnoliaJS in 2019. You can find the slides for that on my GitHub. </p>
+    <p>I spoke about avoiding SUX, or some user experience, at MagnoliaJS in 2019. You can find the slides for that on <a href="https://github.com/kaylasween/accessibility/blob/master/HowtoAvoidSux-MagnoliaJS19.pdf">my GitHub</a>. </p>
     <h3><a href="https://www.renderatl.com">Render(ATL)</a></h3>
     <p>I'll be speaking at Render(ATL) in May 2020 about progressive enhancement for React. I hope to see you there!</p>
     <h2>Meetups</h2>
