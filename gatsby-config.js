bb25d548c10d10c74d16485087c2facbe448ab5a
Kayla Sween <kayla@kaylasween.com>
2020-01-11 18:34:46 -0600

    continuing to remove wip sections of site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,6 @@ module.exports = {
         username: 'kaylasween'
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/blog-posts`,
-        name: `blog-posts`,
-      },
-    },
     `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
