05141a7f06389f94f5983385f8613bbccda6f897
Kayla Sween <kayla@kaylasween.com>
2020-05-28 17:19:03 -0500

    updating what I'm up to. Making the covid graphs take up more of the screen.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,22 @@ import { Helmet } from 'react-helmet'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
+import '../styles/pages/_home.scss'
+
 const IndexPage = () => (
   <Layout>
     <Helmet>
       <title>Home | Kayla Sween</title>
     </Helmet>
-    <SEO title="Home" keywords={[`Kayla`, `Sween`, `Kayla Sween`, `Web Development`, `front end developer`, `UX`, `accessibility`]} />
-    <h1>Hi! I'm Kayla. I am a front-end developer, powerlifter, and dog mom.</h1>
-    <p>
-      This site is currently a work-in-progress, but feel free to poke around!
+    <SEO title="Home" keywords={[`Kayla`, `Sween`, `Kayla Sween`, `Web Development`, `front end developer`, `UX`, `UX Engineer`]} />
+    <div className="home">
+      <h1>Hi! I'm Kayla. I am a front-end engineer, powerlifter, and dog mom.</h1>
+      <p>
+        This site is currently a work-in-progress, but feel free to poke around!
       To see what I'm working on, visit <a href="https://github.com/kaylasween">my GitHub</a> to see what I'm working on now.
     </p>
-    <p>I'm also starting to add some visualizations for COVID-19 cases in Mississippi. You can visit the <Link to="/COVID-19/">COVID-19 section of my site</Link> to see those.</p>
+      <p>I'm also starting to add some visualizations for COVID-19 cases in Mississippi. You can visit the <Link to="/COVID-19/">COVID-19 section of my site</Link> to see those.</p>
+    </div>
   </Layout>
 )
 
--- src/pages/talks.js
+++ src/pages/talks.js
@@ -8,29 +8,36 @@ import MagnoliaJS from '../images/MagnoliaJS.jpeg'
 const Talks = () => (
   <Layout>
     <Helmet>
-      <title>Talks | Kayla Sween</title>
+      <title>Teaching | Kayla Sween</title>
     </Helmet>
-    <h1>Talks</h1>
+    <h1>Teaching</h1>
+    <h2>Talks</h2>
     <div class="row">
       <img src={MagnoliaJS} alt="Kayla speaking at MagnoliaJS 2019. Her talk was 'How to Avoid SUX'." />
       <div>
-        <h2>Conferences</h2>
-        <h3><a href="https://magnoliajs.com">MagnoliaJS</a></h3>
-        <h4>2019</h4>
+        <h3>Conferences</h3>
+        <h4><a href="https://magnoliajs.com">MagnoliaJS</a></h4>
+        <h5>2019</h5>
         <p>I spoke about avoiding SUX, or some user experience, at MagnoliaJS in 2019.
           You can find the slides for <a href="https://github.com/kaylasween/accessibility/blob/master/HowtoAvoidSux-MagnoliaJS19.pdf">"How to Avoid SUX" on my GitHub</a>.</p>
-        <h4>2020</h4>
+        <h5>2020</h5>
         <p>I spoke about progressive enhancement in React at MagnoliaJS 2020. This conference was virtual due to the COVID-19 pandemic, but fortunately, that means
             it was recorded! You can watch <a href="https://youtu.be/BC4xOOk2VUY?t=7167">Progressive Enhancement for React on Youtube</a>.
           </p>
-        <h3><a href="https://www.renderatl.com">Render(ATL)</a></h3>
-        <p>I'll be speaking at Render(ATL) in May 2020 about progressive enhancement for React. I hope to see you there!</p>
+        <h4><a href="https://www.renderatl.com">Render(ATL)</a></h4>
+        <p>I'll be speaking at Render(ATL) in August 2020 about progressive enhancement for React. I hope to see you there!</p>
       </div>
     </div>
-    <h2>Meetups</h2>
-    <h3><a href="https://jacksondevs.tech">Jackson Area Web and App Developers (JAWAD) Meetup</a></h3>
+    <h3>Meetups</h3>
+    <h4><a href="https://jacksondevs.tech">Jackson Area Web and App Developers (JAWAD) Meetup</a></h4>
     <p>You can find the <a href="https://github.com/kaylasween/JAWAD-Pour/blob/master/pour.png">slides for my talk about P.O.U.R.</a> at the July 2019 JAWAD
     Meetup on Github.</p>
+    <h2>Instructing</h2>
+    <h3><a href="https://egghead.io/instructors/kayla-sween">egghead.io</a></h3>
+    <p>As of May 2020, I have started publishing content on <a href="https://egghead.io/">egghead.io</a>. You can check out my lessons on <a href="https://egghead.io/instructors/kayla-sween">my instructor page</a>!</p>
+    <h2>Blog Posts</h2>
+    <h3><a href="https://dev.to/kaylasween">Dev.to</a></h3>
+    <p>I sometimes write blog posts for the Dev Community. You can check those out by going to <a href="https://dev.to/kaylasween">my Dev.to profile</a>!</p>
   </Layout>
 )
 
