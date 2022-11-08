dca26c61a6b78a567390181a0f53cfeb0b2a1237
Kayla Sween <kayla.huddleston@me.com>
2022-11-07 18:47:01 -0600

    fixing indentation issues.

--- src/components/CodeBlock.svelte
+++ src/components/CodeBlock.svelte
@@ -5,6 +5,13 @@
     $: ({value} = portableText)
 </script>
   
-<pre>
-    <code>{value.code}</code>
-</pre>
+<pre class="code-block"><code>{value.code}</code></pre>
+
+<style>
+    .code-block {
+        background-color: black;
+        color: white;
+        overflow: scroll;
+        padding: 1rem;
+    }
+</style>
