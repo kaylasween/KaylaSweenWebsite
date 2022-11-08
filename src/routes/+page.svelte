6d63f50e8be9342d79b87edc7983d9aaec533181
Kayla Sween <kayla.huddleston@me.com>
2022-11-07 19:06:10 -0600

    changing some text on the home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </svelte:head>
 
 <h1>Hey! I'm Kayla Sween.</h1>
-<p>I'm currently rebuilding my website using Svelte. Come back later!</p>
+<p>I'm currently rebuilding my website using Svelte, so it's a little bare for now.</p>
 
 <p>In the meantime, you can check out my <a href="https://dev.to/kayla">Dev blog</a>, <a href="https://egghead.io/q/resources-by-kayla-sween">Egghead videos</a>, or the conference that I organize, <a href="https://magnoliajs.com">MagnoliaJS</a>.</p>
 
