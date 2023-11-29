b39cdaf38e168bf52d1de84a63a1c982ef98dbf7
Kayla Sween <kayla.huddleston@me.com>
2023-11-29 17:16:52 -0600

    fixing how the post list gets all cramped when there are more than 3 but I don't love the way it is right now.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,38 +1,39 @@
 <script>
-    import formatDate from '../../lib/formatDate'
+  import formatDate from '../../lib/formatDate'
 
-    export let data
-    export let posts = data.posts
+  export let data
+  export let posts = data.posts
 </script>
 
 <svelte:head>
-	<title>Blog | Kayla Sween</title>
+  <title>Blog | Kayla Sween</title>
 </svelte:head>
-    
+
 <h1>Blog</h1>
 <ul class="blog-list">
-    {#each posts as post}
-        <li class="blog-list-item">
-            <a href={post.slug.current}>{post.title}</a>
-            {formatDate(post.publishedAt)}
-        </li>
-    {/each}
+  {#each posts as post}
+    <li class="blog-list-item">
+      <a href={post.slug.current}>{post.title}</a>
+      {formatDate(post.publishedAt)}
+    </li>
+  {/each}
 </ul>
 
 <style>
-    .blog-list {
-        display: flex;
-        justify-content: space-evenly;
-        padding-left: 0;
-    }
+  .blog-list {
+    display: flex;
+    justify-content: space-evenly;
+    padding-left: 0;
+    flex-flow: wrap;
+  }
 
-    .blog-list-item {
-        border: 1px solid white;
-        display: flex;
-        flex-direction: column;
-        list-style-type: none;
-        margin: 1rem;
-        padding: 1rem;
-        width: 50%;
-    }
+  .blog-list-item {
+    border: 1px solid white;
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+    margin: 1rem;
+    padding: 1rem;
+    max-width: 75vw;
+  }
 </style>
