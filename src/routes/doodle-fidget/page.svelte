bbe885fb4bbc234b88213c0a1cb49836e3e53d33
Kayla Sween <kayla.huddleston@me.com>
2024-02-21 11:54:34 -0600

    adding a privacy policy page for doodlefidget app.

---
+++ src/routes/doodle-fidget/page.svelte
@@ -0,0 +1,7 @@
+<h1>Doodle Fidget Privacy Policy</h1>
+<p>
+  DoodleFidget respects your privacy and is committed to protecting your
+  personal information. We want you to feel confident while using our app. We do
+  not store <em>any</em> personal information on our servers. All information is
+  stored locally on your device and will be deleted if the app is deleted.
+</p>
