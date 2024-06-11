db5cbaab9ecb016864e8713d1e8e73725c9458c2
Kayla Sween <kayla.huddleston@me.com>
2024-06-11 01:18:54 -0400

    adding sticker link and blurb.

--- src/routes/(landing)/links/+page.svelte
+++ src/routes/(landing)/links/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Github from '../../../assets/github.svelte'
   import GoogleDrive from '../../../assets/google-drive.svelte'
+  import JavaScript from '../../../assets/javascript.svelte'
   import LinkedIn from '../../../assets/linkedin.svelte'
   import Kayla from '../../../assets/kayla.jpeg'
   import Watch from '../../../assets/watch.svelte'
@@ -8,24 +9,35 @@
 
 <img src={Kayla} alt="Kayla Sween" />
 <h1>Kayla Sween</h1>
+<small
+  >I've got 9 years of experience in front-end development with 2 years of
+  technical leadership experience. I'm open for senior front-end engineer,
+  technical lead, or first-time engineering management roles!</small
+>
 <ul>
   <li>
     <a
       href="https://drive.google.com/file/d/1eLBnkfCg9BtV37Knv7CVHzSOt2LxfiVu/view?usp=sharing"
     >
       <GoogleDrive />
-      &nbsp;Resume</a
+      <span>Resume</span></a
     >
   </li>
   <li>
     <a href="https://www.linkedin.com/in/kayla-sween/"
-      ><LinkedIn />&nbsp;LinkedIn</a
+      ><LinkedIn /><span>LinkedIn</span></a
     >
   </li>
   <li><a href="https://github.com/kaylasween"><Github />&nbsp;GitHub</a></li>
   <li>
     <a href="https://apps.apple.com/us/app/doodlefidget/id6477495440"
-      ><Watch />&nbsp;DoodleFidget</a
+      ><Watch /><span>DoodleFidget</span></a
+    >
+  </li>
+  <li>
+    <a
+      href="https://www.etsy.com/listing/1306932844/anti-javascript-javascript-club-stickers"
+      ><JavaScript /><span>Anti JS JS Club Stickers</span></a
     >
   </li>
 </ul>
@@ -40,11 +52,11 @@
   }
 
   h1 {
-    margin-bottom: 1rem;
+    margin-bottom: 0.5rem;
   }
 
   ul {
-    margin: 2rem auto;
+    margin: 1rem auto;
     padding-left: 0;
     width: max-content;
   }
@@ -64,6 +76,10 @@
     font-size: 1.25rem;
     padding: 1rem;
     text-decoration: none;
+  }
+
+  a * {
+    margin-left: 0.5rem;
   }
 
   a:hover {
